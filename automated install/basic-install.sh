c3c83e4fb640c382a44de7811d621a889c035e90
OpenLock <74428999+OpenLock20@users.noreply.github.com>
2023-01-16 11:22:06 -0600

    Up 3

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -74,7 +74,7 @@ webroot="/var/www/html"
 # Two notable scripts are gravity.sh (used to generate the HOSTS file) and advanced/Scripts/webpage.sh (used to install the Web admin interface)
 webInterfaceGitUrl="https://github.com/OpenLock20/Public-Test-Inter.git"
 webInterfaceDir="${webroot}/admin"
-piholeGitUrl="https://github.com/pi-hole/pi-hole.git"
+piholeGitUrl="https://github.com/OpenLock20/Public-Master-Test.git"
 PI_HOLE_LOCAL_REPO="/etc/.pihole"
 # List of pihole scripts, stored in an array
 PI_HOLE_FILES=(chronometer list piholeDebug piholeLogFlush setupLCD update version gravity uninstall webpage)
